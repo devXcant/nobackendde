58f0306310a080ca62a8951a176ec4ad4e91038c
devXcant <devxcant@gmail.com>
2024-05-08 12:50:48 +0100

    code update

--- EXPRESS WITH NODE/01CRUD/app.js
+++ EXPRESS WITH NODE/01CRUD/app.js
@@ -1,154 +1,135 @@
 const express = require('express');
-const fs = require('fs')
+const fs = require('fs');
 const app = express();
-let movies = JSON.parse(fs.readFileSync('./data/movies.json'))
+const morgan = require('morgan');
 
-const logger = function(req, res, next){
-    console.log('custom middleware called')
+// Load movies data from JSON file
+let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Custom logger middleware
+const logger = function(req, res, next) {
+    console.log('Custom middleware called');
+    next();
 }
-
-app.use(express.json())
 app.use(logger);
-app.use((req,res,next) => {
-    req.requestedAt = new Date().toISOString()
-    next()
-})
 
-// ROUTE HANDLER FUNCTIONS
-const getAllMovies = (req,res)=> {
+// Middleware to add requestedAt timestamp
+app.use((req, res, next) => {
+    req.requestedAt = new Date().toISOString();
+    next();
+});
+
+// Route handler functions
+const getAllMovies = (req, res) => {
     res.status(200).json({
-        status: "success",
-        // count:movies.length,
+        status: 'success',
         requestedAt: req.requestedAt,
         data: {
             movies: movies
         }
-    })
+    });
 }
 
-const getMoviesById = (req,res)=> {
-    // console.log(req.params)
+const getMoviesById = (req, res) => {
     const id = req.params.id * 1;
-
-    let movie = movies.find( el => el.id ===id);
-    if(!movie){
+    let movie = movies.find(el => el.id === id);
+    if (!movie) {
         return res.status(404).json({
-            status: "error",
-            message: 'movie with '+id+' not found'
-        })
+            status: 'error',
+            message: 'Movie with ID ' + id + ' not found'
+        });
     }
-
-    //send movie in the response
     res.status(200).json({
-        status: "success",
+        status: 'success',
         data: {
             movie: movie
         }
-    })
+    });
 }
 
-const postMovies = (req,res)=> {
-    console.log(req.body)
-    const newId = movies[movies.length - 1].id + 1;
-
-    const newMovie = Object.assign({id: newId},req.body)
-
-    movies.push(newMovie)
-    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err)=> {
+const postMovies = (req, res) => {
+    const newId = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
+    const newMovie = { id: newId, ...req.body };
+    movies.push(newMovie);
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
+        if (err) {
+            return res.status(500).json({
+                status: 'error',
+                message: 'Internal server error'
+            });
+        }
         res.status(201).json({
-            status: "success",
+            status: 'success',
             data: {
                 movie: newMovie
             }
-        })
-    })
-    // res.send('created')
+        });
+    });
 }
 
-
-const patchMovies = (req, res)=> {
-    let id = req.params.id * 1
-    let movieToUpdate = movies.find(el => el.id === id )
-    if(!movieToUpdate){
-        res.status(404).json({
-            status: 'fail',
-            message: 'No movie witih ID `+id+`` is found'
-        })
+const patchMovies = (req, res) => {
+    const id = req.params.id * 1;
+    let movieToUpdate = movies.find(el => el.id === id);
+    if (!movieToUpdate) {
+        return res.status(404).json({
+            status: 'error',
+            message: 'Movie with ID ' + id + ' not found'
+        });
     }
-    let index = movies.indexOf(movieToUpdate)
-
-    Object.assign(movieToUpdate, req.body)
-
-    movies[index]= movieToUpdate;
-    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err)=> {
-         res.status(200).json({
-            status: "success",
+    Object.assign(movieToUpdate, req.body);
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
+        if (err) {
+            return res.status(500).json({
+                status: 'error',
+                message: 'Internal server error'
+            });
+        }
+        res.status(200).json({
+            status: 'success',
             data: {
                 movie: movieToUpdate
             }
-        })
-    })
+        });
+    });
 }
 
-const deleteMovies = (req,res) => {
+const deleteMovies = (req, res) => {
     const id = req.params.id * 1;
-    const movieToDelete = movies.find(el=> el.id ===id);
-
-    if(!movieToDelete){
-        
-            return res.status(404).json({
-                status: 'fail',
-                message: 'No movie witih ID `+id+` is found to delete'
-        }) 
+    const index = movies.findIndex(el => el.id === id);
+    if (index === -1) {
+        return res.status(404).json({
+            status: 'error',
+            message: 'Movie with ID ' + id + ' not found'
+        });
     }
-
-    const index = movies.indexOf(movieToDelete)
-
-
     movies.splice(index, 1);
-
-    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err)=> {
-         res.status(204).json({
-            status: "success",
-            data: {
-                movie: null
-            }
-        })
-    })
-
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
+        if (err) {
+            return res.status(500).json({
+                status: 'error',
+                message: 'Internal server error'
+            });
+        }
+        res.status(204).json({
+            status: 'success',
+            data: null
+        });
+    });
 }
 
-// GET - api/movies
-// app.get('/api/v1/movies', getAllMovies )
+// Routes
+app.get('/api/v1/movies', getAllMovies);
+app.get('/api/v1/movies/:id', getMoviesById);
+app.post('/api/v1/movies', postMovies);
+app.patch('/api/v1/movies/:id', patchMovies);
+app.delete('/api/v1/movies/:id', deleteMovies);
 
-
-// GET - api/v1/movie/id
-// app.get('/api/v1/movies/:id', getMoviesById )
-
-//POST - api/movies
-// app.post('/api/v1/movies', postMovie)
-
-// PUT $ PATCH
-// PATCH A RESOURCE
-// app.patch('/api/v1/movies/:id', patchMovies)
-
-// DELETE A RESOURCE
-// app.delete('/api/v1/movies/:id', deleteMovies)
-
-
-app.route('/api/v1/movies')
-    .get(getAllMovies)
-    .post(postMovies)
-
-
-app.route('/api/v1/movies/:id')
-    .get(getMoviesById)
-    .patch(patchMovies)
-    .delete(deleteMovies)
-
-
-//CREATE A SERVER
+// Start server
 const port = 3000;
-app.listen(port, ()=> {
-    console.log('watching you..')
-})
+app.listen(port, () => {
+    console.log('im watching you on ' + port);
+});
